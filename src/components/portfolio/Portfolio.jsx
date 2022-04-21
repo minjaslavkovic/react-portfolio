96dffdd4e12ac5493a10de2eeb57ec85492ee9f1
Minja Slavkovic <minja.slavkovic@gmail.com>
2022-04-21 22:35:46 +0100

    Adding portfolio and images

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.png'
+import IMG2 from '../../assets/portfolio2.png'
+import IMG3 from '../../assets/portfolio3.png'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container container-portfolio">
+        <article className='portfolio-item'>
+          <div className="portfolio-item-image">
+            <img src={IMG1} alt="BandMates" />
+          </div>
+          <h3>Title</h3>
+          <a href="https://github.com" className='btn' target='_blank' rel="noopener noreferrer">Github</a>
+          <a href="https://github.com" className='btn btn-primary' target='_blank' rel="noopener noreferrer">Live Demo</a>
+        </article>
+        <article className='portfolio-item'>
+          <div className="portfolio-item-image">
+            <img src={IMG2} alt="VenuLet" />
+          </div>
+          <h3>Title</h3>
+          <a href="https://github.com" className='btn' target='_blank' rel="noopener noreferrer">Github</a>
+          <a href="https://github.com" className='btn btn-primary' target='_blank' rel="noopener noreferrer">Live Demo</a>
+        </article>
+        <article className='portfolio-item'>
+          <div className="portfolio-item-image">
+            <img src={IMG3} alt="Watch List" />
+          </div>
+          <h3>Title</h3>
+          <a href="https://github.com" className='btn' target='_blank' rel="noopener noreferrer">Github</a>
+          <a href="https://github.com" className='btn btn-primary' target='_blank' rel="noopener noreferrer">Live Demo</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
