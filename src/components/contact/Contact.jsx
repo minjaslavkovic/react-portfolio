20ce8da5b7a1ffe5b44990667e43294df19dbb1e
Minja Slavkovic <minja.slavkovic@gmail.com>
2022-04-23 11:41:09 +0100

    Added contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import './contact.css'
+import {FiMail} from 'react-icons/fi'
+import {MdPhoneIphone} from 'react-icons/md'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact-container">
+        <div className="contact-options">
+          <article className='contact-option'>
+            <FiMail className='contact-option-icon'/>
+            <h4>Email</h4>
+            <h5>minja.slavkovic@gmail.com</h5>
+            <a href="mailto:minja.slavkovic@gmail.com">Send a message</a>
+          </article>
+          <article className='contact-option'>
+            <MdPhoneIphone className='contact-option-icon'/>
+            <h4>Phone</h4>
+            <h5>+447568775528</h5>
+            <a href="mailto:minja.slavkovic@gmail.com" target='_blank' rel="noopener noreferrer">Contact</a>
+          </article>
+        </div>
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="text" name="email" placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
