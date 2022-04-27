f44ed00bf87588efa8f06d2ec0724afcc39fa51d
Minja Slavkovic <minja.slavkovic@gmail.com>
2022-04-27 16:34:44 +0100

    Added footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+import {BsFacebook} from 'react-icons/bs'
+import {BsInstagram} from 'react-icons/bs'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer-logo'>MS</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer-scoails">
+        <a href="https://linkedin.com"><BsLinkedin/></a>
+        <a href="https://github.com"><BsGithub/></a>
+        <a href="https://facebook.com"><BsFacebook/></a>
+        <a href="https://instagram.com"><BsInstagram/></a>
+      </div>
+    </footer>
   )
 }
 
